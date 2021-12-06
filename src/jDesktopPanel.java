9bb8140c28c9fcc35da51f17c3e328a04ae60ea2
SadeeshaChathura98SCC <95602228+SadeeshaChathura98SCC@users.noreply.github.com>
2021-12-06 11:17:54 +0530

    Add files via upload

---
+++ src/jDesktopPanel.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Asus
+ */
+class jDesktopPanel {
+
+    static void removeAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void updateUI() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static Object add(homepage HM) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
